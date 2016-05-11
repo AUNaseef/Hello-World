6d8aa5baa358bc0604b5336c1b35aa9665d38aee
Abdullah Umar Naseef <AUNaseef@users.noreply.github.com>
2016-05-11 12:43:27 +0530

    Create infiniteLoop.cpp
    I haven't tested this code.

---
+++ CPlusPlus/infiniteLoop.cpp
@@ -0,0 +1,15 @@
+// an endless program. 
+#include <iostream>
+using namespace std;
+
+int main(){
+   int number;
+   int total;
+   for(,,){
+     cout << endl << "Total is" << total;
+     cout << "enter number :";
+     cin >> number;
+     total += number;
+   }
+ 
+} 
