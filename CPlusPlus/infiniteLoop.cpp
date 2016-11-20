95d868eac3ca3b562bfcdad8500be999516fe03f
Abdullah Umar Naseef <AUNaseef@users.noreply.github.com>
2016-11-21 00:26:59 +0530

    Update infiniteLoop.cpp

--- CPlusPlus/infiniteLoop.cpp
+++ CPlusPlus/infiniteLoop.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
    int number;
    int total;
-   for(,,){
+   for(;;){
      cout << endl << "Total is" << total;
      cout << "enter number :";
      cin >> number;
